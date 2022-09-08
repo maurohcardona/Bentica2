1acf7b7e226a25b00428ee40f3363807c2a11e15
mauro.cardona <maurohcardona@gmail.com>
2022-09-07 21:09:38 -0300

    final, ponele

--- carrito.js
+++ carrito.js
@@ -1,10 +1,5 @@
 
 
-
-
-
-
-
 let carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
 document.getElementById("cantidadCarrito").innerHTML = carrito.length ?? 0;
 
@@ -17,19 +12,20 @@ const ProductoAMostrar = JSON.parse(localStorage.getItem('verProducto')) ?? ""
 
   
     
-
+let filtroPrecio= ""
+let precioTotal =""
+let carritoSinDuplicados =[]
+let miItem =[]
+let numeroUnidadesItem = []
  
-  
-
- //if (JSON.stringify(carrito) === JSON.stringify(unicos)){
 function randerizarCarrito() {
- carrito.forEach((producto) => {
-    
-    const idButton = `add-cart${producto.id}`;
+
+  carrito.forEach((producto) => {
+  const idButton = `add-cart${producto.id}`;
     document.getElementById("seccion-carrito").innerHTML += `<div class='card m-2'>
     <p>${producto.nombre}</p>
-    <p><img class="fotocarrito"${producto.imagen}</p>
-    <div><input id="contador${producto.id}" class="cantidadcarrito" type="number" value ="1">
+    <p><img class="fotocarrito" src="${producto.imagen}" </p>
+    <div><input id="contador${producto.id}" class="cantidadcarrito" type="number" value ="1"></input>
     <button onclick="sumar(${producto.id})"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
   </svg></button>
@@ -37,13 +33,21 @@ function randerizarCarrito() {
   <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
 </svg></button>
   </div>
-    <p>Precio: ${producto.precio}</p> 
+    <p>Precio: ${producto.precio}  </p> 
     <button onclick="sacarDelCarrito(${producto.id})" id="sacar${producto.id}">Sacar del carrito</button>
     </div>`
     
 })
+if (carrito.length > 0){
+  filtroPrecio = carrito.map((e) => {return e.precio}) 
+  precioTotal = filtroPrecio.reduce((a,b) => {return a+b})
+  document.getElementById("carritoTotal").innerHTML = `Total = $${precioTotal}`
+}
+else{
+  document.getElementById("carritoTotal").innerHTML =  `Total = $0`
 }
 
+}
 
 
 
@@ -60,18 +64,12 @@ function restar(id) {
 
 
 const agregarAlCarrito = (producto) => {
-  unicos = carrito.map(e => e.id)
- if (unicos.includes(producto.id)) {
-  console.log("Producto duplicado")
-    
- }
-  else {
   carrito.push(producto);
   document.getElementById("cantidadCarrito").innerHTML = carrito.length;
   localStorage.setItem('carrito', JSON.stringify(carrito))
  randerizarCarrito() 
  location. reload()
-}
+
 }
 
 
@@ -87,28 +85,7 @@ function sacarDelCarrito(id) {
     localStorage.setItem('carrito', JSON.stringify(carrito))
     document.getElementById("seccion-carrito").innerHTML = `<div></div>`
     randerizarCarrito()
-    /*carrito.forEach((producto) => {
     
-        const idButton = `add-cart${producto.id}`;
-        document.getElementById("seccion-carrito").innerHTML += `<div class='card m-2'>
-        <p>${producto.nombre}</p>
-        <p><img class="fotocarrito"${producto.imagen}</p>
-        <div><input id="contador${producto.id}" class="cantidadcarrito" type="number" value ="1">
-        <button onclick="sumar(${producto.id})"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-      </svg></button>
-      <button onclick="restar(${producto.id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
-      <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
-    </svg></button>
-      </div>
-        <p>Precio: ${producto.precio}</p> 
-        <button onclick="sacarDelCarrito(${producto.id})" id="sacar${producto.id}">Sacar del carrito</button>
-        </div>`
-        
-    })
-    if (carrito.length === 0) {
-      document.getElementById("seccion-carrito").innerHTML = `<div>Carrito vacio</div>`
-    }*/
 }
     
  
@@ -119,16 +96,22 @@ function vaciarcarrito() {
         document.getElementById("cantidadCarrito").innerHTML = carrito.length ?? 0;
         document.getElementById("seccion-carrito").innerHTML = `<div>Carrito vacio</div>`
         localStorage.removeItem('carrito')
+        randerizarCarrito()
     }
     
 }
 
 
+
+
+
+
+
 randerizarCarrito()
 
 document.getElementById('titulo').innerHTML = ProductoAMostrar.nombre || "";
-document.getElementById('imagen').innerHTML = ProductoAMostrar.imagen || "";
-
+document.getElementById('imagen').innerHTML = `<img class ="fotitos-productos" src=${ProductoAMostrar.imagen}>` || "";
+document.getElementById('precio').innerHTML =`Precio: $${ProductoAMostrar.precio}` || "";
  
 
 
